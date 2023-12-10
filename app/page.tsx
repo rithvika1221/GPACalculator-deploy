763f2503dcb9f2fdc7a110e758113de54050cb9e
Rithvika Devisetti <70250513+rithvika1221@users.noreply.github.com>
2023-12-10 01:16:24 -0800

    Logo changes
    Updated the Logo Description

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
 
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Hello This is Rithvika, Welcome to fun nextjs project.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
